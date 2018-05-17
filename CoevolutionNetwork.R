a22585de3bc44112190d7bd16a8f9757684ad0c1
DylanHong <Dylan.P.Hong.19@dartmouth.edu>
2018-05-17 01:07:16 -0400

    Code Update

--- CoevolutionNetwork.R
+++ CoevolutionNetwork.R
@@ -78,17 +78,11 @@ finalAC <- finalAC[ , !(names(finalAC) %in% drops)]
 
 df <- finalAC
 df <- df[,c(1,2,8,14)]
-names1 <- df$ACTOR1
-names2 <- df$ACTOR2
-names <- list(names1,names2)
-totalNames <- unique(c(names1,names2))
 
 query_year <- function(df,y1,y2,y3,y4){
   tempdf <- df[df$YEAR == y1 | df$YEAR == y2 | df$YEAR == y3 | df$YEAR == y4,]
   return(tempdf)
 }
-
-
 
 names_list <- function(df){
   names1 <- df$ACTOR1
@@ -98,15 +92,7 @@ names_list <- function(df){
   return(totalNames)
 }
 
-newdf <- query_year(df,2000,2004,2008,2012)
-namesList <- names_list(newdf)
-
-
 create_matrices_new <- function(df,y1,y2,y3,y4){
-  y1 <- 2000
-  y2 <- 2004
-  y3 <- 2008
-  y4 <- 2012
   years <- c(y1,y2,y3,y4)
   
   newdf <- query_year(df,y1,y2,y3,y4)
@@ -172,99 +158,18 @@ create_matrices_new <- function(df,y1,y2,y3,y4){
 
 godList <- create_matrices_new(df,2000,2004,2008,2012)
 
-godList1 <- godList[[1]]
-godList2 <- godList[[2]]
+masterAlly <- godList[[1]]
+masterConflict <- godList[[2]]
 
+allyt1 <- masterAlly[[1]]
+allyt2 <- masterAlly[[2]]
+allyt3 <- masterAlly[[3]]
+allyt4 <- masterAlly[[4]]
 
-row_check <- function(mconflict,mally,df1,row){
-  apply(df1,1,function(row){
-    
-  })
-}
-
-#Get the dataframe sorted by years
-dataByYear <- list()
-for (i in c(1997:2018)){
-  yearTemp <- finalAC[finalAC$YEAR == i,]
-  dataByYear[[i-1996]] <- yearTemp
-}
-
-####  Reading Data
-''
-# Read the van de Bunt data.
-# For meaning and codes, consult the Siena website or the Siena manual.
-setwd("/Users/junzhao/Downloads/vdBunt_data")
-library(RSiena)
-vdb.w0 <- as.matrix(read.table("VRND32T0.DAT"))
-vdb.w1 <- as.matrix(read.table("VRND32T1.DAT"))
-vdb.w2 <- as.matrix(read.table("VRND32T2.DAT"))
-vdb.w3 <- as.matrix(read.table("VRND32T3.DAT"))
-vdb.w4 <- as.matrix(read.table("VRND32T4.DAT"))
-vdb.w5 <- as.matrix(read.table("VRND32T5.DAT"))
-vdb.w6 <- as.matrix(read.table("VRND32T6.DAT"))
-vdb.attr <- as.matrix(read.table("VARS.DAT"))
-
-# Recode 6 and 9 to missing.
-
-vdb.w0[vdb.w0 %in% c(6,9)] <- NA
-vdb.w1[vdb.w1 %in% c(6,9)] <- NA
-vdb.w2[vdb.w2 %in% c(6,9)] <- NA
-vdb.w3[vdb.w3 %in% c(6,9)] <- NA
-vdb.w4[vdb.w4 %in% c(6,9)] <- NA
-vdb.w5[vdb.w5 %in% c(6,9)] <- NA
-vdb.w6[vdb.w6 %in% c(6,9)] <- NA
-
-# Recode 4 (acquaintance) and 5 (difficult) to no tie
-
-vdb.w0[vdb.w0 %in% c(4,5)] <- 0
-vdb.w1[vdb.w1 %in% c(4,5)] <- 0
-vdb.w2[vdb.w2 %in% c(4,5)] <- 0
-vdb.w3[vdb.w3 %in% c(4,5)] <- 0
-vdb.w4[vdb.w4 %in% c(4,5)] <- 0
-vdb.w5[vdb.w5 %in% c(4,5)] <- 0
-vdb.w6[vdb.w6 %in% c(4,5)] <- 0
-
-# Networks with ordered values for the ties are represented
-# in RSiena by using with different dichotomizations of the
-# valued networks.
-# This leads to multiple dependent networks
-# that satisfy order relations.
-# The network constructed now has tie values 0, 1, 2, 3.
-# The "friendly relation" with values {1, 2, 3}
-# is obtained by recoding:
-
-vdb.w0.th3 <- vdb.w0
-vdb.w1.th3 <- vdb.w1
-vdb.w2.th3 <- vdb.w2
-vdb.w3.th3 <- vdb.w3
-vdb.w4.th3 <- vdb.w4
-vdb.w5.th3 <- vdb.w5
-vdb.w6.th3 <- vdb.w6
-vdb.w0.th3[vdb.w0 %in% c(1,2,3)] <- 1
-vdb.w1.th3[vdb.w1 %in% c(1,2,3)] <- 1
-vdb.w2.th3[vdb.w2 %in% c(1,2,3)] <- 1
-vdb.w3.th3[vdb.w3 %in% c(1,2,3)] <- 1
-vdb.w4.th3[vdb.w4 %in% c(1,2,3)] <- 1
-vdb.w5.th3[vdb.w5 %in% c(1,2,3)] <- 1
-vdb.w6.th3[vdb.w6 %in% c(1,2,3)] <- 1
-
-# The "friend" relation for values {1, 2} by recoding:
-
-vdb.w0.th2 <- vdb.w0.th3
-vdb.w1.th2 <- vdb.w1.th3
-vdb.w2.th2 <- vdb.w2.th3
-vdb.w3.th2 <- vdb.w3.th3
-vdb.w4.th2 <- vdb.w4.th3
-vdb.w5.th2 <- vdb.w5.th3
-vdb.w6.th2 <- vdb.w6.th3
-
-vdb.w0.th2[vdb.w0 == 3] <- 0
-vdb.w1.th2[vdb.w1 == 3] <- 0
-vdb.w2.th2[vdb.w2 == 3] <- 0
-vdb.w3.th2[vdb.w3 == 3] <- 0
-vdb.w4.th2[vdb.w4 == 3] <- 0
-vdb.w5.th2[vdb.w5 == 3] <- 0
-vdb.w6.th2[vdb.w6 == 3] <- 0
+conflictt1 <- masterConflict[[1]]
+conflictt2 <- masterConflict[[2]]
+conflictt3 <- masterConflict[[3]]
+conflictt4 <- masterConflict[[4]]
 
 # We do not use the "close friend" relation here, because there are
 # very few threshold crossings between values 1 and 2.
@@ -276,41 +181,41 @@ vdb.w6.th2[vdb.w6 == 3] <- 0
 # there is evidence for reciprocity, but the parameter estimate
 # is not very well determined by the data.)
 
-library(RSiena)
-friendly2345 <- sienaDependent(
-  array(c(vdb.w2.th3, vdb.w3.th3, vdb.w4.th3, vdb.w5.th3),
-        dim=c(32, 32, 4)))
-friends2345  <- sienaDependent(
-  array(c(vdb.w2.th2 ,vdb.w3.th2, vdb.w4.th2, vdb.w5.th2),
-        dim=c(32, 32, 4)))
+allySiena <- sienaDependent(
+  array(c(allyt1, allyt2, allyt3, allyt4),
+        dim=c(1921, 1921, 4)))
+conflictSiena  <- sienaDependent(
+  array(c(conflictt1 ,conflictt2, conflictt3, conflictt4),
+        dim=c(1921, 1921, 4)))
 
 # Attributes:
-sex          <- coCovar(vdb.attr[,1])
-program      <- coCovar(vdb.attr[,2])
-smoke        <- coCovar(vdb.attr[,3])
+# sex          <- coCovar(vdb.attr[,1])
+# program      <- coCovar(vdb.attr[,2])
+# smoke        <- coCovar(vdb.attr[,3])
 
 # Data set and effects:
-vdb.ordered2345 <- sienaDataCreate(friendly2345, friends2345,
-                                   sex, program, smoke)
+vdb.ordered2345 <- sienaDataCreate(allySiena, conflictSiena)
 ##############
 myCoEvolutionEff <- getEffects(vdb.ordered2345) 
 
 # network dynamic (inPopSqrt)
 myCoEvolutionEff <- includeEffects(myCoEvolutionEff, transTrip,transTies, cycle3) 
-myCoEvolutionEff <- includeEffects(myCoEvolutionEff, name = "friendly2345", egoX, altX, simX,
+#myCoEvolutionEff <- includeEffects(myCoEvolutionEff, name = "allySiena", egoX, altX, simX,
                                    interaction1="sex" )
 
-myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "friendly2345", from, interaction1 = "friends2345" )
-myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "friendly2345", to, interaction1 = "friends2345" )
-myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "friendly2345", sharedIn, interaction1 = "friends2345" )
-myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "friendly2345", cl.XWX, interaction1 = "friends2345" )
+myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "allySiena", from, interaction1 = "conflictSiena" )
+myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "allySiena", to, interaction1 = "conflictSiena" )
+myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "allySiena", sharedIn, interaction1 = "conflictSiena" )
+myCoEvolutionEff <- includeEffects( myCoEvolutionEff, name = "allySiena", cl.XWX, interaction1 = "conflictSiena" )
 
 #myCoEvolutionEff <- includeInteraction(myCoEvolutionEff,effFrom, totSim, name = "depressionbeh", interaction1 = c("sex", "friendship" ) )
 myCoEvolutionEff
 
-myCoEvAlgorithm <- sienaAlgorithmCreate(projname = 'All girls-w35-dep4-7', seed =1568)
-GroupsModel <- sienaModelCreate(projname = 'All girls-w35-dep4-7')
-modelTEST <- siena07(myCoEvAlgorithm, data = vdb.ordered2345, effects = myCoEvolutionEff)
+myCoEvAlgorithm <- sienaAlgorithmCreate(projname = 'PleaseForTheLoveOfGodWork', seed =1568,
+                                        n3 = 500)
+GroupsModel <- sienaModelCreate(projname = 'PleaseForTheLoveOfGodWork')
+modelTEST <- siena07(myCoEvAlgorithm, data = vdb.ordered2345, effects = myCoEvolutionEff,
+                     useCluster=TRUE, nbrNodes=3)
 summary(modelTEST)
 
 modelTEST2 <- siena07(myCoEvAlgorithm, data = vdb.ordered2345, effects = myCoEvolutionEff, 
